c3721f316b150b9758501efa6c54bbee7dee39fa
minkai <951286607@qq.com>
2023-08-22 16:23:07 +0800

    初始化

---
+++ websocket-item/src/main/java/com/example/websocketitem/WebsocketItemApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketItemApplication.class, args);
+	}
+
+}
---
+++ websocket-item/src/test/java/com/example/websocketitem/WebsocketItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
