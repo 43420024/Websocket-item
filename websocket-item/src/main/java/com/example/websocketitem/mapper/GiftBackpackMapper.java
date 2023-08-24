8d281ae159b522bd883616e1dbff7c6ec0404e94
yanghang <1339160008@qq.com>
2023-08-24 09:28:06 +0800

    yh:购买礼物

---
+++ websocket-item/src/main/java/com/example/websocketitem/mapper/GiftBackpackMapper.java
@@ -0,0 +1,18 @@
+package com.example.websocketitem.mapper;
+
+import com.example.websocketitem.domain.GiftBackpack;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author cd
+* @description 针对表【tcd_gift_backpack】的数据库操作Mapper
+* @createDate 2023-08-24 08:49:02
+* @Entity com.example.websocketitem.domain.GiftBackpack
+*/
+public interface GiftBackpackMapper extends BaseMapper<GiftBackpack> {
+
+}
+
+
+
+
