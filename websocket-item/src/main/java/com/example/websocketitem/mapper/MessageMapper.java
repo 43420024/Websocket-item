03c07a65aa8c684c0943af4e14c52e5906bdc5d8
yanghang <1339160008@qq.com>
2023-08-30 15:12:52 +0800

    yh:消息

---
+++ websocket-item/src/main/java/com/example/websocketitem/mapper/MessageMapper.java
@@ -0,0 +1,18 @@
+package com.example.websocketitem.mapper;
+
+import com.example.websocketitem.model.Message;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author cd
+* @description 针对表【tcd_message(信息表)】的数据库操作Mapper
+* @createDate 2023-08-30 10:00:31
+* @Entity com.example.websocketitem.model.Message
+*/
+public interface MessageMapper extends BaseMapper<Message> {
+
+}
+
+
+
+
--- websocket-item/src/main/java/com/example/websocketitem/model/User.java
+++ websocket-item/src/main/java/com/example/websocketitem/model/User.java
@@ -4,15 +4,18 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
 import java.io.Serializable;
 import java.util.Date;
+
 import lombok.Data;
 
 /**
  * 用户表
+ *
  * @TableName tcd_user
  */
-@TableName(value ="tcd_user")
+@TableName(value = "tcd_user")
 @Data
 public class User implements Serializable {
     /**
@@ -56,6 +59,12 @@ public class User implements Serializable {
      */
     @TableField(value = "status")
     private Integer status;
+
+    /**
+     * 该账号的父账号
+     */
+    @TableField(value = "parent_id")
+    private Long parentId;
 
     @TableField(exist = false)
     private UserInfo userInfo;
