4e4afcadadb5040818088864f7fd6931a6b3f644
xiaoxiaoqian111 <3111219941@qq.com>
2023-08-30 10:27:37 +0800

    如实公布

---
+++ websocket-item/src/main/java/com/example/websocketitem/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.websocketitem.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/*
+@Component
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private PermissionInterceptor permissionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(permissionInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/user/logon");
+    }
+}
+*/
---
+++ websocket-item/src/main/java/com/example/websocketitem/interceptor/PermissionInterceptor.java
@@ -0,0 +1,21 @@
+package com.example.websocketitem.interceptor;
+
+/*@Component
+public class PermissionInterceptor implements HandlerInterceptor {
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userNumber = request.getHeader("userNumber");
+        return this.userNumberAll(Long.parseLong(userNumber));
+    }
+    public boolean userNumberAll(Long userNumber){
+        if(userMapper.user(userNumber)==null){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+}*/
